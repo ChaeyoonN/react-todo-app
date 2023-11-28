f172e21f15670fda8727deb25f466f214b859da6
chaeyoon <codbs0703@naver.com>
2023-11-28 14:30:26 +0900

    #10. 로그인 기능 구현(Context API 사용)

--- src/components/todo/TodoTemplate.js
+++ src/components/todo/TodoTemplate.js
@@ -26,7 +26,7 @@ const TodoTemplate = () => {
     부모 컴포넌트에서 함수를 선언 (매개변수 꼭 선언) -> props로 함수를 전달
     자식 컴포넌트에서 전달받은 함수를 호출하면서 매개값으로 데이터를 전달.
   */
-  const addTodo = (todoText) => {
+  const addTodo = async (todoText) => {
     const newTodo = {
       // id: makeNewId(),
       title: todoText,
@@ -44,15 +44,24 @@ const TodoTemplate = () => {
 
     // setTodos([...todos, newTodo]);
 
-    fetch(API_BASE_URL, {
+    const res = await fetch(API_BASE_URL, {
       method: 'POST',
       headers: { 'content-type': 'application/json' },
       body: JSON.stringify(newTodo),
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        setTodos(json.todos);
-      });
+    });
+
+    const json = await res.json();
+    setTodos(json.todos);
+
+    // fetch(API_BASE_URL, {
+    //   method: 'POST',
+    //   headers: { 'content-type': 'application/json' },
+    //   body: JSON.stringify(newTodo),
+    // })
+    //   .then((res) => res.json())
+    //   .then((json) => {
+    //     setTodos(json.todos);
+    //   });
   };
 
   // 할 일 삭제 처리 함수
--- src/components/user/Join.js
+++ src/components/user/Join.js
@@ -112,7 +112,7 @@ const Join = () => {
         }
       })
       .then((json) => {
-        // console.log(json);
+        console.log(json);
         if (json) {
           msg = '이메일이 중복되었습니다.';
         } else {
